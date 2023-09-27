bd235e8260e081026da23fc70e00171ad53fcdaa
tinyngoshosh <146202883+tinyngoshosh@users.noreply.github.com>
2023-09-27 16:49:57 +0300

    Voting

---
+++ voting(0).py
@@ -0,0 +1,8 @@
+#code to check if a person is eligible to vote
+age=int(input("please enter age:"))
+=input("enter nationality:")
+if age>=18 and Kenyan:
+    print("Eligible to vote")
+else:
+    print("Oops! you are not elegible")
+    
